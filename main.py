24b32a8f774e8b4f2b30c1534a55d8848491e44c
[Rubben23] <[rubynel120@gmail.com]>
2021-09-30 14:46:08 +0300

    Am reusit sa rezol problemele, insa nu am reusit sa fac un meniu de tip consola

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ Returneaza true daca n este prim si false daca nu.
 '''
 def is_prime(n):
   # codul vostru aici
+  if n < 2:
+    return False
+  for i in range (2 , n//2+1 ):
+      if n % i == 0:
+        return False
+  return True
   
   
 '''
@@ -10,6 +16,10 @@ Returneaza produsul numerelor din lista lst.
 '''
 def get_product(lst):
   # codul vostru aici
+  p = 1
+  for i in range(len(lst)):
+    p *= lst[i]
+  return p
   
   
 '''
@@ -17,6 +27,12 @@ Returneaza CMMDC a doua numere x si y folosind primul algoritm.
 '''
 def get_cmmdc_v1(x, y):
   # codul vostru aici
+  while x > y or y < x:
+    if x > y:
+      x = x - y
+    else:
+      y = y - x
+  return x
   
   
 '''
@@ -24,8 +40,13 @@ Returneaza CMMDC a doua numere x si y folosind al doilea algoritm.
 '''
 def get_cmmdc_v2(x, y):
   # codul vostru aici
-  
-  
+  while y != 0:
+    r = x % y
+    x = y
+    y = r
+  return x
+
+
 def main():
   # interfata de tip consola aici
 
